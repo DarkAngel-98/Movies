b0f2405de57da3f68e0941abf4eb8ae7f615dabb
darko <darko@nteams.com>
2024-09-13 12:55:39 +0200

    log

--- app/src/main/java/com/example/mymovies/ui/main/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/mymovies/ui/main/fragments/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.example.mymovies.ui.main.fragments.home
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -55,6 +56,7 @@ class HomeFragment : Fragment(), CategoryClick {
                     it.category?.let { category ->
                         presentMovieByCategory(category)
                     }
+                    Log.d("Collect", "Collecting")
                 }
             }
         }
